1b9c5398a5f0e571a74c5df45ef67accc680266b
Nazar Svystula <nazarsvistula11@gmail.com>
2025-02-08 00:14:39 +0200

    Created sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
-/ @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      './templates//*.html'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		'./templates/**/*.html'
+	],
+	theme: {
+		extend: {
+			colors: {
+				'accent': {
+					100: '#92C7CF',
+					200: '#b0d7d9'
+				},
+				'primary': '#FBF9F1',
+				'secondary': '#E5E1DA',
+				'dark': '#2D2D2D',
+			}
+		},
+	},
+	plugins: [],
 }
+

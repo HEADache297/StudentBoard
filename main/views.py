f1b0b4e4e68b3b8846d35a04ced6ace346ff9e21
Bohdan337 <bogdansem37@gmail.com>
2025-02-08 22:55:44 +0200

    added Event and Group models, implemented group creation

--- main/views.py
+++ main/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import EventForm, GroupForm
 
 # Create your views here.
 def calendar(request):
     return render(request, 'calendar.html')
 
 def groupes(request):
-    return render(request, 'groupes.html')
+    if request.method == 'POST':
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('groupes')
+    else:
+        form = GroupForm()
+
+    return render(request, 'groupes.html', {'form': form})
 
 def inbox(request):
     return render(request, 'inbox.html')

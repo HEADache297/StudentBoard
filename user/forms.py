9a5ab81f7ebf067f4c80ad4d934a63780c2cf672
Nazar Svystula <nazarsvistula11@gmail.com>
2025-02-10 21:55:47 +0200

    Created styles for registration forms

--- user/forms.py
+++ user/forms.py
@@ -1,13 +1,23 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms.widgets import Input, PasswordInput, EmailInput
 from .models import CustomUser
 
 class CustomUserRegistrationForm(UserCreationForm):
+    name = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-secondary text-dark', 'placeholder': 'Name'}))
+    surname = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-secondary text-dark', 'placeholder': 'Surname'}))
+    email = forms.CharField(max_length=255, widget=EmailInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-secondary text-dark', 'placeholder': 'Email'}))
+    password1 = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-secondary text-dark', 'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-secondary text-dark', 'placeholder': 'Check password'}))
+
     class Meta:
         model = CustomUser
         fields = ('email', 'name', 'surname', 'password1', 'password2')
 
 class CustomUserLoginForm(AuthenticationForm):
-     class Meta:
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-secondary text-dark', 'placeholder': 'Email'}), label='Email')    
+    password = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-secondary text-dark', 'placeholder': 'Password'}))
+
+    class Meta:
         model = CustomUser
         fields = ('email', 'password')
